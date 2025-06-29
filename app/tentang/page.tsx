e827e26be12120b4729d84f6e8c461324d6352e9
alifahket <alifah@politeknikpgribanten.ac.id>
2025-06-29 12:36:56 +0700

    Create page.tsx

---
+++ app/tentang/page.tsx
@@ -0,0 +1,8 @@
+export default function TentangKami() {
+  return (
+    <div className="prose dark:prose-invert mx-auto">
+      <h2>Tentang Kami</h2>
+      <p>Kami adalah travel penyelenggara program tabungan emas untuk keberangkatan Umroh dan Haji ONH Plus...</p>
+    </div>
+  );
+}
