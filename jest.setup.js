5a0f519fcf744caf2847d0a663c4157a53693b82
alifahket <alifah@politeknikpgribanten.ac.id>
2025-06-29 10:11:09 +0700

    Create jest.setup.js

---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+// jest.setup.js
+import '@testing-library/jest-dom/extend-expect';
+
+// Jika butuh custom global mocks, bisa ditambahkan di sini.
+// Misalnya:
+// global.fetch = require('jest-fetch-mock');
