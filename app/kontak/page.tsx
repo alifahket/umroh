07a5990a57120f502cc83fe0e32c002e8ae32f2b
alifahket <alifah@politeknikpgribanten.ac.id>
2025-06-29 12:39:32 +0700

    Create page.tsx

---
+++ app/kontak/page.tsx
@@ -0,0 +1,9 @@
+export default function KontakKami() {
+  return (
+    <div className="prose dark:prose-invert mx-auto">
+      <h2>Kontak</h2>
+      <p>Email: info@travelumroh.id</p>
+      <p>WhatsApp: 0812-3456-7890</p>
+    </div>
+  );
+}
